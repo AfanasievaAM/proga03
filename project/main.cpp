d5f361897016526a1136f52d6cca045fcfec2df7
Arina (AfanasievaAM) <arinafanasieva@mail.ru>
2020-05-22 18:45:19 +0300

    обработка ошибок

--- project/main.cpp
+++ project/main.cpp
@@ -132,12 +132,18 @@ int main(int argc, char* argv[])
 {
     if (argc > 1)
     {
-         CURL *curl = curl_easy_init();
+        CURL *curl = curl_easy_init();
         if(curl)
         {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            res = CURLE_FAILED_INIT;
+            if (res != CURLE_OK)
+            {
+                cout << curl_easy_strerror(res) << endl;
+                exit(1);
+            }
             curl_easy_cleanup(curl);
         }
         return (0);
